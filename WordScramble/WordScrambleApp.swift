8ee6081ad83e5551d61347a200cc2a419315049b
kqDevs <facundovogel23@gmail.com>
2024-08-04 03:47:51 -0300

    First commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Facundo Vogel on 30/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
